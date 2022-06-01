37ebbfcab8bba0f142c49f655ef3d67b67a4aa98
Oliver Rockstedt <info@sourcebox.de>
2022-06-01 18:25:11 +0200

    Dummy implementation for particle engine

--- src/dsp/engine/particle_engine.rs
+++ src/dsp/engine/particle_engine.rs
@@ -2,4 +2,32 @@
 
 // Based on MIT-licensed code (c) 2016 by Emilie Gillet (emilie.o.gillet@gmail.com)
 
-// TODO: implement
+use super::{note_to_frequency, Engine, EngineParameters};
+
+#[derive(Debug, Default)]
+pub struct ParticleEngine {
+    // TODO: implement
+}
+
+impl ParticleEngine {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Engine for ParticleEngine {
+    fn init(&mut self) {
+        // TODO: implement
+    }
+
+    #[inline]
+    fn render(
+        &mut self,
+        parameters: &EngineParameters,
+        out: &mut [f32],
+        aux: &mut [f32],
+        _already_enveloped: &mut bool,
+    ) {
+        // TODO: implement
+    }
+}
