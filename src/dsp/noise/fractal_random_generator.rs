0e93df020a15446d6f9d193e0ee66e2324b0dec3
Oliver Rockstedt <info@sourcebox.de>
2022-06-03 16:38:52 +0200

    Removed unused fractal random generator module.

--- src/dsp/noise/fractal_random_generator.rs
+++
@@ -1,8 +0,0 @@
-//! Stack octaves of a noise source to generate a fractal noise.
-
-// Based on MIT-licensed code (c) 2016 by Emilie Gillet (emilie.o.gillet@gmail.com)
-
-#[derive(Debug, Default)]
-pub struct FractalRandomGenerator {
-    // TODO: implement
-}
--- src/dsp/noise/mod.rs
+++ src/dsp/noise/mod.rs
@@ -1,5 +1,4 @@
 pub mod clocked_noise;
 pub mod dust;
-pub mod fractal_random_generator;
 pub mod particle;
 pub mod smooth_random_generator;
