171c49f70d76fc3706a5ea3a8977e72f7fda715b
Oliver Rockstedt <info@sourcebox.de>
2022-06-03 16:27:36 +0200

    Diffuser dummy implementation

--- src/dsp/fx/diffuser.rs
+++ src/dsp/fx/diffuser.rs
@@ -2,4 +2,30 @@
 
 // Based on MIT-licensed code (c) 2016 by Emilie Gillet (emilie.o.gillet@gmail.com)
 
-// TODO: implement
+use core::alloc::GlobalAlloc;
+
+#[derive(Debug)]
+pub struct Diffuser {
+    // Todo: implement
+}
+
+impl Diffuser {
+    pub fn new<T: GlobalAlloc>(buffer_allocator: &T) -> Self {
+        Self {
+        // Todo: implement
+        }
+    }
+
+    pub fn init(&mut self) {
+        // Todo: implement
+    }
+
+    pub fn clear(&mut self) {
+        // Todo: implement
+    }
+
+    #[inline]
+    pub fn process(&mut self, amount: f32, rt: f32, in_out: &mut [f32]) {
+        // Todo: implement
+    }
+}
