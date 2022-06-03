545ad05d6443ac4a1e75035b2089768e4132a12f
Oliver Rockstedt <info@sourcebox.de>
2022-06-03 16:34:10 +0200

    Test for particle engine

--- tests/engines/particle_engine.rs
+++ tests/engines/particle_engine.rs
@@ -10,15 +10,111 @@ const BLOCK_SIZE: usize = 24;
 
 #[test]
 fn particle_engine_harmonics() {
-    // TODO: implement
+    let mut engine = particle_engine::ParticleEngine::new(&std::alloc::System, BLOCK_SIZE);
+    let mut out = [0.0; BLOCK_SIZE];
+    let mut aux = [0.0; BLOCK_SIZE];
+    let mut wav_data = Vec::new();
+    let mut wav_data_aux = Vec::new();
+
+    engine.init();
+
+    let duration = 2.0;
+    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
+    let mut already_enveloped = false;
+
+    for n in 0..blocks {
+        let parameters = EngineParameters {
+            trigger: if n % (blocks / 5) == 0 {
+                TriggerState::RisingEdge
+            } else {
+                TriggerState::Low
+            },
+            note: 48.0,
+            timbre: 0.5,
+            morph: 0.5,
+            harmonics: modulation::ramp_up(n, blocks),
+            accent: 1.0,
+        };
+
+        engine.render(&parameters, &mut out, &mut aux, &mut already_enveloped);
+        wav_data.extend_from_slice(&out);
+        wav_data_aux.extend_from_slice(&aux);
+    }
+
+    wav_writer::write("engines/particle/particle_harmonics.wav", &wav_data).ok();
+    wav_writer::write("engines/particle/particle_harmonics_aux.wav", &wav_data_aux).ok();
 }
 
 #[test]
 fn particle_engine_timbre() {
-    // TODO: implement
+    let mut engine = particle_engine::ParticleEngine::new(&std::alloc::System, BLOCK_SIZE);
+    let mut out = [0.0; BLOCK_SIZE];
+    let mut aux = [0.0; BLOCK_SIZE];
+    let mut wav_data = Vec::new();
+    let mut wav_data_aux = Vec::new();
+
+    engine.init();
+
+    let duration = 2.0;
+    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
+    let mut already_enveloped = false;
+
+    for n in 0..blocks {
+        let parameters = EngineParameters {
+            trigger: if n % (blocks / 5) == 0 {
+                TriggerState::RisingEdge
+            } else {
+                TriggerState::Low
+            },
+            note: 48.0,
+            timbre: modulation::ramp_up(n, blocks),
+            morph: 0.5,
+            harmonics: 0.5,
+            accent: 1.0,
+        };
+
+        engine.render(&parameters, &mut out, &mut aux, &mut already_enveloped);
+        wav_data.extend_from_slice(&out);
+        wav_data_aux.extend_from_slice(&aux);
+    }
+
+    wav_writer::write("engines/particle/particle_timbre.wav", &wav_data).ok();
+    wav_writer::write("engines/particle/particle_timbre_aux.wav", &wav_data_aux).ok();
 }
 
 #[test]
 fn particle_engine_morph() {
-    // TODO: implement
+    let mut engine = particle_engine::ParticleEngine::new(&std::alloc::System, BLOCK_SIZE);
+    let mut out = [0.0; BLOCK_SIZE];
+    let mut aux = [0.0; BLOCK_SIZE];
+    let mut wav_data = Vec::new();
+    let mut wav_data_aux = Vec::new();
+
+    engine.init();
+
+    let duration = 2.0;
+    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
+    let mut already_enveloped = false;
+
+    for n in 0..blocks {
+        let parameters = EngineParameters {
+            trigger: if n % (blocks / 5) == 0 {
+                TriggerState::RisingEdge
+            } else {
+                TriggerState::Low
+            },
+            note: 48.0,
+            timbre: 0.5,
+            morph: modulation::ramp_up(n, blocks),
+            harmonics: 0.5,
+            accent: 1.0,
+        };
+
+        engine.render(&parameters, &mut out, &mut aux, &mut already_enveloped);
+        wav_data.extend_from_slice(&out);
+        wav_data_aux.extend_from_slice(&aux);
+    }
+
+    wav_writer::write("engines/particle/particle_morph.wav", &wav_data).ok();
+    wav_writer::write("engines/particle/particle_morph_aux.wav", &wav_data_aux).ok();
 }
