b3654bd862946fe70e7ecc08e4b127a59fa5a5e3
Oliver Rockstedt <info@sourcebox.de>
2022-06-06 15:44:26 +0200

    Test for diffuser

--- tests/fx.rs
+++ tests/fx.rs
@@ -11,6 +11,31 @@ use mi_plaits_dsp::dsp::SAMPLE_RATE;
 const BLOCK_SIZE: usize = 24;
 
 #[test]
+fn diffuser() {
+    let amount = 1.0;
+    let rt = 0.5;
+    let duration = 1.0;
+
+    let mut fx = diffuser::Diffuser::new();
+    let mut in_out = [0.0; BLOCK_SIZE];
+    let mut wav_data = Vec::new();
+    fx.init();
+
+    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
+
+    for n in 0..blocks {
+        in_out.fill(0.0);
+        if n == 0 {
+            in_out[0] = 1.0;
+        }
+        fx.process(amount, rt, &mut in_out);
+        wav_data.extend_from_slice(&in_out);
+    }
+
+    wav_writer::write("fx/diffuser.wav", &wav_data).ok();
+}
+
+#[test]
 fn sample_rate_reducer() {
     let frequency = 110.0;
     let duration = 2.0;
