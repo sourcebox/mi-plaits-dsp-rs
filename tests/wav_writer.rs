40c779f2ff97ce2a7ccf772fd8038e93e5762562
Oliver Rockstedt <info@sourcebox.de>
2025-03-15 13:38:05 +0100

    Replaced deprecated wav dependency by hound

--- tests/wav_writer.rs
+++ tests/wav_writer.rs
@@ -1,17 +1,35 @@
 //! Writer for WAV files
 
-use std::fs::File;
 use std::path::Path;
+
+use hound::*;
 
 use mi_plaits_dsp::dsp::SAMPLE_RATE;
 
-pub fn write(filename: &str, data: &[f32]) -> std::io::Result<()> {
-    let path = format!("./out/{filename}");
+/// Writes sample data as WAV file in 32-bit float format.
+pub fn write(
+    filename: impl AsRef<std::path::Path> + core::fmt::Display,
+    samples: &[f32],
+) -> std::io::Result<()> {
+    let path = format!("out/{filename}");
     let path = Path::new(path.as_str());
+
+    // Create parent directories to the path if they don't exist.
     let parent = path.parent().unwrap();
     std::fs::create_dir_all(parent).ok();
-    let mut file = File::create(path)?;
-    let header = wav::Header::new(wav::WAV_FORMAT_IEEE_FLOAT, 1, SAMPLE_RATE as u32, 32);
-    wav::write(header, &wav::BitDepth::from(Vec::from(data)), &mut file)?;
+
+    let spec = WavSpec {
+        channels: 2,
+        sample_rate: SAMPLE_RATE as u32,
+        bits_per_sample: 32,
+        sample_format: SampleFormat::Float,
+    };
+    let mut writer = WavWriter::create(path, spec).unwrap();
+
+    for sample in samples {
+        writer.write_sample(*sample).unwrap();
+        writer.write_sample(*sample).unwrap();
+    }
+
     Ok(())
 }
