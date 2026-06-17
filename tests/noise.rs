2d9f1a34a2d270c7dc92fab80039ca1e0f1dcff8
Oliver Rockstedt <info@sourcebox.de>
2026-06-17 14:24:19 +0200

    Tests: render noises at different sample rates

--- tests/noise.rs
+++ tests/noise.rs
@@ -5,7 +5,6 @@ mod common;
 use common::*;
 use mi_plaits_dsp::noise::*;
 
-const SAMPLE_RATE: f32 = 48000.0;
 const BLOCK_SIZE: usize = 24;
 
 #[test]
@@ -13,20 +12,23 @@ fn clocked_noise() {
     let frequency = 10.0;
     let duration = 1.0;
 
-    let mut noise = clocked_noise::ClockedNoise::new();
-    let mut out = [0.0; BLOCK_SIZE];
-    let mut wav_data = Vec::new();
-    noise.init();
+    for sample_rate in SAMPLE_RATES {
+        let mut noise = clocked_noise::ClockedNoise::new();
+        let mut out = [0.0; BLOCK_SIZE];
+        let mut wav_data = Vec::new();
+        noise.init();
 
-    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
-    let f = frequency / SAMPLE_RATE;
+        let blocks = (duration * sample_rate as f32 / (BLOCK_SIZE as f32)) as usize;
+        let f = frequency / sample_rate as f32;
 
-    for _ in 0..blocks {
-        noise.render(false, f, &mut out);
-        wav_data.extend_from_slice(&out);
+        for _ in 0..blocks {
+            noise.render(false, f, &mut out);
+            wav_data.extend_from_slice(&out);
+        }
+
+        let filename = format!("noises/clocked/clocked_{sample_rate}.wav");
+        write_wav(filename, &wav_data, sample_rate).ok();
     }
-
-    write_wav("noises/clocked/clocked.wav", &wav_data, SAMPLE_RATE as u32).ok();
 }
 
 #[test]
@@ -34,17 +36,20 @@ fn dust() {
     let frequency = 20.0;
     let duration = 1.0;
 
-    let mut wav_data = Vec::new();
+    for sample_rate in SAMPLE_RATES {
+        let mut wav_data = Vec::new();
 
-    let samples = (duration * SAMPLE_RATE) as usize;
-    let f = frequency / SAMPLE_RATE;
+        let samples = (duration * sample_rate as f32) as usize;
+        let f = frequency / sample_rate as f32;
 
-    for _ in 0..samples {
-        let out = dust::dust(f);
-        wav_data.push(out);
+        for _ in 0..samples {
+            let out = dust::dust(f);
+            wav_data.push(out);
+        }
+
+        let filename = format!("noises/dust/dust_{sample_rate}.wav");
+        write_wav(filename, &wav_data, sample_rate).ok();
     }
-
-    write_wav("noises/dust/dust.wav", &wav_data, SAMPLE_RATE as u32).ok();
 }
 
 #[test]
@@ -56,37 +61,32 @@ fn particle() {
     let q = 0.9;
     let duration = 1.0;
 
-    let mut noise = particle::Particle::new();
-    let mut out = [0.0; BLOCK_SIZE];
-    let mut aux = [0.0; BLOCK_SIZE];
-    let mut wav_data = Vec::new();
-    let mut wav_data_aux = Vec::new();
-    noise.init();
+    for sample_rate in SAMPLE_RATES {
+        let mut noise = particle::Particle::new();
+        let mut out = [0.0; BLOCK_SIZE];
+        let mut aux = [0.0; BLOCK_SIZE];
+        let mut wav_data = Vec::new();
+        let mut wav_data_aux = Vec::new();
+        noise.init();
 
-    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
-    let f = frequency / SAMPLE_RATE;
+        let blocks = (duration * sample_rate as f32 / (BLOCK_SIZE as f32)) as usize;
+        let f = frequency / sample_rate as f32;
 
-    for n in 0..blocks {
-        out.fill(0.0);
-        aux.fill(0.0);
-        let sync = n % (blocks / 5) == 0;
-        noise.render(sync, density, gain, f, spread, q, &mut out, &mut aux);
-        wav_data.extend_from_slice(&out);
-        wav_data_aux.extend_from_slice(&aux);
+        for n in 0..blocks {
+            out.fill(0.0);
+            aux.fill(0.0);
+            let sync = n % (blocks / 5) == 0;
+            noise.render(sync, density, gain, f, spread, q, &mut out, &mut aux);
+            wav_data.extend_from_slice(&out);
+            wav_data_aux.extend_from_slice(&aux);
+        }
+
+        let filename = format!("noises/particle/particle_{sample_rate}.wav");
+        write_wav(filename, &wav_data, sample_rate).ok();
+
+        let filename_aux = format!("noises/particle/particle_aux_{sample_rate}.wav");
+        write_wav(filename_aux, &wav_data_aux, sample_rate).ok();
     }
-
-    write_wav(
-        "noises/particle/particle.wav",
-        &wav_data,
-        SAMPLE_RATE as u32,
-    )
-    .ok();
-    write_wav(
-        "noises/particle/particle_aux.wav",
-        &wav_data_aux,
-        SAMPLE_RATE as u32,
-    )
-    .ok();
 }
 
 #[test]
@@ -94,22 +94,21 @@ fn smooth_random_generator() {
     let frequency = 10.0;
     let duration = 1.0;
 
-    let mut osc = smooth_random_generator::SmoothRandomGenerator::new();
-    let mut wav_data = Vec::new();
-    osc.init();
+    for sample_rate in SAMPLE_RATES {
+        let mut osc = smooth_random_generator::SmoothRandomGenerator::new();
+        let mut wav_data = Vec::new();
+        osc.init();
 
-    let samples = (duration * SAMPLE_RATE) as usize;
-    let f = frequency / SAMPLE_RATE;
+        let samples = (duration * sample_rate as f32) as usize;
+        let f = frequency / sample_rate as f32;
 
-    for _ in 0..samples {
-        let out = osc.render(f);
-        wav_data.push(out);
+        for _ in 0..samples {
+            let out = osc.render(f);
+            wav_data.push(out);
+        }
+
+        let filename =
+            format!("noises/smooth_random_generator/smooth_random_generator_{sample_rate}.wav");
+        write_wav(filename, &wav_data, sample_rate).ok();
     }
-
-    write_wav(
-        "noises/smooth_random_generator/smooth_random_generator.wav",
-        &wav_data,
-        SAMPLE_RATE as u32,
-    )
-    .ok();
 }
