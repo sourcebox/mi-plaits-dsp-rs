4b9f20201c9d7ccde73b968b0265f7797c6ce814
Oliver Rockstedt <info@sourcebox.de>
2026-06-17 14:32:24 +0200

    Tests: render fx at different sample rates

--- tests/fx.rs
+++ tests/fx.rs
@@ -6,7 +6,6 @@ use common::*;
 use mi_plaits_dsp::fx::*;
 use mi_plaits_dsp::oscillator::sine_oscillator::SineOscillator;
 
-const SAMPLE_RATE: f32 = 48000.0;
 const BLOCK_SIZE: usize = 24;
 
 #[test]
@@ -15,23 +14,26 @@ fn diffuser() {
     let rt = 0.5;
     let duration = 1.0;
 
-    let mut fx = diffuser::Diffuser::new();
-    let mut in_out = [0.0; BLOCK_SIZE];
-    let mut wav_data = Vec::new();
-    fx.init(SAMPLE_RATE);
+    for sample_rate in SAMPLE_RATES {
+        let mut fx = diffuser::Diffuser::new();
+        let mut in_out = [0.0; BLOCK_SIZE];
+        let mut wav_data = Vec::new();
+        fx.init(sample_rate as f32);
 
-    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
+        let blocks = (duration * sample_rate as f32 / (BLOCK_SIZE as f32)) as usize;
 
-    for n in 0..blocks {
-        in_out.fill(0.0);
-        if n == 0 {
-            in_out[0] = 1.0;
+        for n in 0..blocks {
+            in_out.fill(0.0);
+            if n == 0 {
+                in_out[0] = 1.0;
+            }
+            fx.process(amount, rt, &mut in_out);
+            wav_data.extend_from_slice(&in_out);
         }
-        fx.process(amount, rt, &mut in_out);
-        wav_data.extend_from_slice(&in_out);
-    }
 
-    write_wav("fx/diffuser/diffuser.wav", &wav_data, SAMPLE_RATE as u32).ok();
+        let filename = format!("fx/diffuser/diffuser_{sample_rate}.wav");
+        write_wav(filename, &wav_data, sample_rate).ok();
+    }
 }
 
 #[test]
@@ -39,39 +41,34 @@ fn ensemble() {
     let frequency = 220.0;
     let duration = 2.0;
 
-    let mut osc = SineOscillator::new();
-    let mut fx = ensemble::Ensemble::new();
-    let mut left = [0.0; BLOCK_SIZE];
-    let mut wav_data_left = Vec::new();
-    let mut wav_data_right = Vec::new();
-    osc.init();
-    fx.init();
-    fx.set_amount(0.5);
-    fx.set_depth(0.5);
+    for sample_rate in SAMPLE_RATES {
+        let mut osc = SineOscillator::new();
+        let mut fx = ensemble::Ensemble::new();
+        let mut left = [0.0; BLOCK_SIZE];
+        let mut wav_data_left = Vec::new();
+        let mut wav_data_right = Vec::new();
+        osc.init();
+        fx.init();
+        fx.set_amount(0.5);
+        fx.set_depth(0.5);
 
-    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
-    let f = frequency / SAMPLE_RATE;
+        let blocks = (duration * sample_rate as f32 / (BLOCK_SIZE as f32)) as usize;
+        let f = frequency / sample_rate as f32;
 
-    for _ in 0..blocks {
-        osc.render(f, &mut left);
-        let mut right = left.clone();
-        fx.process(&mut left, &mut right);
-        wav_data_left.extend_from_slice(&left);
-        wav_data_right.extend_from_slice(&right);
+        for _ in 0..blocks {
+            osc.render(f, &mut left);
+            let mut right = left.clone();
+            fx.process(&mut left, &mut right);
+            wav_data_left.extend_from_slice(&left);
+            wav_data_right.extend_from_slice(&right);
+        }
+
+        let filename_left = format!("fx/ensemble/ensemble_left_{sample_rate}.wav");
+        write_wav(filename_left, &wav_data_left, sample_rate).ok();
+
+        let filename_right = format!("fx/ensemble/ensemble_right_{sample_rate}.wav");
+        write_wav(filename_right, &wav_data_right, sample_rate).ok();
     }
-
-    write_wav(
-        "fx/ensemble/ensemble_left.wav",
-        &wav_data_left,
-        SAMPLE_RATE as u32,
-    )
-    .ok();
-    write_wav(
-        "fx/ensemble/ensemble_right.wav",
-        &wav_data_right,
-        SAMPLE_RATE as u32,
-    )
-    .ok();
 }
 
 #[test]
@@ -79,29 +76,27 @@ fn sample_rate_reducer() {
     let frequency = 110.0;
     let duration = 2.0;
 
-    let mut osc = SineOscillator::new();
-    let mut fx = sample_rate_reducer::SampleRateReducer::new();
-    let mut in_out = [0.0; BLOCK_SIZE];
-    let mut wav_data = Vec::new();
-    osc.init();
-    fx.init();
+    for sample_rate in SAMPLE_RATES {
+        let mut osc = SineOscillator::new();
+        let mut fx = sample_rate_reducer::SampleRateReducer::new();
+        let mut in_out = [0.0; BLOCK_SIZE];
+        let mut wav_data = Vec::new();
+        osc.init();
+        fx.init();
 
-    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
-    let f = frequency / SAMPLE_RATE;
+        let blocks = (duration * sample_rate as f32 / (BLOCK_SIZE as f32)) as usize;
+        let f = frequency / sample_rate as f32;
 
-    for n in 0..blocks {
-        osc.render(f, &mut in_out);
-        let fx_f = mod_ramp_up(n, blocks) * 0.1;
-        fx.process(fx_f, &mut in_out, true);
-        wav_data.extend_from_slice(&in_out);
+        for n in 0..blocks {
+            osc.render(f, &mut in_out);
+            let fx_f = mod_ramp_up(n, blocks) * 0.1;
+            fx.process(fx_f, &mut in_out, true);
+            wav_data.extend_from_slice(&in_out);
+        }
+
+        let filename = format!("fx/sample_rate_reducer/sample_rate_reducer_{sample_rate}.wav");
+        write_wav(filename, &wav_data, sample_rate).ok();
     }
-
-    write_wav(
-        "fx/sample_rate_reducer/sample_rate_reducer.wav",
-        &wav_data,
-        SAMPLE_RATE as u32,
-    )
-    .ok();
 }
 
 #[test]
@@ -109,22 +104,25 @@ fn overdrive() {
     let frequency = 110.0;
     let duration = 2.0;
 
-    let mut osc = SineOscillator::new();
-    let mut fx = overdrive::Overdrive::new();
-    let mut in_out = [0.0; BLOCK_SIZE];
-    let mut wav_data = Vec::new();
-    osc.init();
-    fx.init();
+    for sample_rate in SAMPLE_RATES {
+        let mut osc = SineOscillator::new();
+        let mut fx = overdrive::Overdrive::new();
+        let mut in_out = [0.0; BLOCK_SIZE];
+        let mut wav_data = Vec::new();
+        osc.init();
+        fx.init();
 
-    let blocks = (duration * SAMPLE_RATE / (BLOCK_SIZE as f32)) as usize;
-    let f = frequency / SAMPLE_RATE;
+        let blocks = (duration * sample_rate as f32 / (BLOCK_SIZE as f32)) as usize;
+        let f = frequency / sample_rate as f32;
 
-    for n in 0..blocks {
-        osc.render(f, &mut in_out);
-        let drive = mod_ramp_up(n, blocks);
-        fx.process(drive, &mut in_out);
-        wav_data.extend_from_slice(&in_out);
+        for n in 0..blocks {
+            osc.render(f, &mut in_out);
+            let drive = mod_ramp_up(n, blocks);
+            fx.process(drive, &mut in_out);
+            wav_data.extend_from_slice(&in_out);
+        }
+
+        let filename = format!("fx/overdrive/overdrive_{sample_rate}.wav");
+        write_wav(filename, &wav_data, sample_rate).ok();
     }
-
-    write_wav("fx/overdrive/overdrive.wav", &wav_data, SAMPLE_RATE as u32).ok();
 }
